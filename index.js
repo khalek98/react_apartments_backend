5794372fec2e96a41ab6bcf29c60f50fac93393a
Khalek <xalek.islam@gmail.com>
2022-09-07 20:05:48 +0300

    connect mongoose

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 import express from 'express';
-// import fs from 'fs';
-// import multer from 'multer';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
 import { createApartPostValidation } from './validations/validations.js';
 import { handleValidatonErrors } from './utils/index.js';
 import * as ApartsController from './controllers/ApartsController.js';
-import dotenv from 'dotenv';
 
-// dotenv.config();
+dotenv.config();
 
 mongoose
-  .connect(process.env.MONGODB_URI)
+  .connect(
+    'mongodb+srv://admin:17supusaH@cluster0.ufyxye3.mongodb.net/apartments?retryWrites=true&w=majority',
+  )
   .then(() => console.log('DB OK!'))
   .catch((err) => console.log('DB Error', err));
 
@@ -20,16 +21,16 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get('/', (req, res) =>
+  res.send('<h1>Data Base for Apartments App by Khalek</h1>'),
+);
+app.get('/aparts', ApartsController.getAllAparts);
 app.post(
   '/aparts',
   createApartPostValidation,
   handleValidatonErrors,
   ApartsController.createApartPost,
 );
-app.get('/aparts', ApartsController.getAllAparts);
-// app.get('/', (req, res) =>
-//   res.send('<h1>Data Base for Apartments App by Khalek</h1>'),
-// );
 
 app.listen(process.env.PORT || 4000, (err) => {
   if (err) {
