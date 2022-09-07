df71b6f87a729621301d5e9527d12564ab236483
Khalek <xalek.islam@gmail.com>
2022-09-08 00:55:14 +0300

    cors fix final

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from 'express';
+import cors from 'cors';
 import mongoose from 'mongoose';
 // import dotenv from 'dotenv';
-import cors from 'cors;';
 
 import { createApartPostValidation } from './validations/validations.js';
 import { handleValidatonErrors } from './utils/index.js';
